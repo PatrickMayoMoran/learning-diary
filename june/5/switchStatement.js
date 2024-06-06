6c056af51bf5b31c443bad52209310d62c2bcb85
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-06-06 23:48:40 +0000

    Add example of switch statement

---
+++ june/5/switchStatement.js
@@ -0,0 +1,14 @@
+let cat = "tiny"
+
+switch (cat) {
+  case "tiny":
+    console.log("I love my cat!");
+    break;
+  case "other cat":
+    console.log("Other cats are nice too!");
+    break;
+  default:
+    console.log("meow");
+}
+
+console.log("Done with cat praising");
